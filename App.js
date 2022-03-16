b23c3a891057ceec54c6019fc6cd3d7c074f569b
soon-404 <tfpsoon@gmail.com>
2022-03-16 09:55:14 +0700

    ios device fix

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, View, SafeAreaView} from 'react-native';
 
 import CarList from './components/CarList';
 import Header from './components/Header';
@@ -7,18 +7,30 @@ import ModalPop from './components/ModalPop';
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <ModalPop modalVisible={modalVisible} setModalVisible={setModalVisible} />
-      <Header modalVisible={modalVisible} setModalVisible={setModalVisible} />
-      <CarList />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerr}>
+        <ModalPop
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
+        <Header modalVisible={modalVisible} setModalVisible={setModalVisible} />
+        <CarList />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  containerr: {
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     backgroundColor: '#fff',
     alignItems: 'center',
